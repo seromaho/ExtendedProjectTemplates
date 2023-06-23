b47eac0fa9e7aa30e945ad87a179e6cba0968965
Sebastian Höft <sebastian.hoeft@multimedial-systeme.de>
2023-06-23 10:21:40 +0200

    Implement DI, implement the file configuration provider (JSON), implement the Environment Variables configuration provider, implement logging w/ { Serilog } (console sink)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace ConsoleApp
+﻿using Microsoft.Extensions.Configuration;
+// Implementation of key-value pair based configuration for Microsoft.Extensions.Configuration.
+// Includes the memory configuration provider.
+using Microsoft.Extensions.DependencyInjection;
+// Default implementation of dependency injection for Microsoft.Extensions.DependencyInjection.
+using Microsoft.Extensions.Hosting;
+// Hosting and startup infrastructures for applications.
+using Serilog;
+// Simple .NET logging with fully-structured events.
+
+namespace ConsoleApp
 {
     internal class Program
     {
+        // Create a Method to ..
+        // .. implement the file configuration provider (JSON)
+        // .. implement the Environment Variables configuration provider
+        /// <summary>
+        /// Modifies a configuration object, using the file configuration provider (JSON) and the Environment Variables configuration provider.
+        /// </summary>
+        static void BuildConfig(IConfigurationBuilder configurationBuilder)
+        {
+            // Modify a configuration object, using the file configuration provider (JSON) and the Environment Variables configuration provider
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Build()).Enrich.FromLogContext().WriteTo.Console().CreateLogger();
+
+            Log.Logger.Information("Starting ...");
+
+            // Implement dependency injection in ~/Program.Main()
+            var host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+                {
+                    // This method gets called by the runtime. Use this method to add services to the container.
+                    services.AddSingleton<IExampleService, StartupService>();
+                }).UseSerilog().Build();
+
+            //host.RunAsync();
+
+            // Use dependency injection in ~/Program.Main()
+            var service = ActivatorUtilities.CreateInstance<StartupService>(host.Services);
+
+            service.GetStartupMessage();
+
+            Log.Logger.Information("Stopping ...");
+
+            Log.CloseAndFlush();
         }
     }
 }
---
+++ ConsoleApp/StartupService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+// Implementation of key-value pair based configuration for Microsoft.Extensions.Configuration.
+// Includes the memory configuration provider.
+using Microsoft.Extensions.Logging;
+// Logging infrastructure default implementation for Microsoft.Extensions.Logging.
+
+namespace ConsoleApp
+{
+    internal class StartupService : IExampleService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<StartupService> _logger;
+
+        /// <summary>
+        /// Instance constructor
+        /// </summary>
+        /// <param name="configuration"></param>
+        public StartupService(IConfiguration configuration, ILogger<StartupService> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the startup message specified in the app settings, and writes it to the standard output stream.
+        /// </summary>
+        public void GetStartupMessage()
+        {
+            // Use the { ILogger } instance from the dependency injection container
+            _logger.LogInformation("Getting startup message from configuration: {startupMessage}",
+                _configuration.GetValue<string>("StartupService:StartupMessage", "Hello World!"));
+
+            // Get values from the configuration object given their key and their target type
+            Console.WriteLine(_configuration
+                .GetSection("StartupService")
+                .GetValue<string>("StartupMessage", "Hello World!"));
+            // Alternative syntax:
+            // Console.WriteLine(_configuration.GetValue<string>("StartupService:StartupMessage", "Hello World!"));
+        }
+    }
+}
